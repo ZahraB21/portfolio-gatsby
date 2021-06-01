cc263b2e67bda15a788a4ac557c7f08c894a8fc7
Zahra <zbehfarshad@gmail.com>
2021-06-01 16:01:38 -0700

    Changing the path to Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { Link, graphql } from 'gatsby'
-import Layout from "../components/Layout"
+import Layout from '../components/Layout/Layout';
 import styles from '../styles/home.module.css'
 import Img from "gatsby-image"
 
 export default function Home({ data }) {
-  console.log(data)
   
   return (
     <Layout>
       <section className={styles.header}>
-        <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
-          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
-        </div>
-        <Img fluid={data.file.childImageSharp.fluid} />
+          <h2>Hello, I'm Zahra Jana Behfarshad</h2>
+          <h3>A Web Developer | Front-End Dev</h3>
+          <Link className={styles.btn} to="/projects">View Work</Link>
+        {/* <Img fluid={data.file.childImageSharp.fluid} /> */}
       </section>
     </Layout>
   )
