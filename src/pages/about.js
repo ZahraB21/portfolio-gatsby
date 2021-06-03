c9bd22df4d3c67c477c111a44b0ae0ffcda7cfe2
Zahra <zbehfarshad@gmail.com>
2021-06-03 13:43:34 -0700

    Adding content to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,11 +8,14 @@ const About = () => {
     <Layout>
       <Box className={styles.aboutSection}>
         <Grid container>
-          <Grid item xs="4">
+          <Grid item xs={5}>
             <Typography variant="h2">About</Typography>
           </Grid>
-          <Grid item xs="8">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis ipsum corrupti nostrum iure ipsam, unde eveniet temporibus eaque distinctio porro, accusamus, cum officia! Numquam, quam dolores praesentium quae sapiente tenetur minima officia reprehenderit cupiditate minus ad animi, sit, cumque omnis earum totam sint maxime? Eveniet voluptatum officiis minus, debitis consectetur voluptas numquam animi magni sequi corporis ipsum odio voluptatibus nulla tempora, sunt aliquid? Ea qui recusandae eius nostrum dolorum saepe laboriosam dolorem molestiae, blanditiis expedita odit obcaecati, amet quia deserunt! Repellat at suscipit expedita vel nam, odit molestiae quo pariatur quod alias, explicabo quibusdam nostrum illo eum fugiat iusto corporis doloremque tempore! Eum molestias fuga dolor praesentium vitae, quaerat doloribus, aspernatur quas fugiat illo voluptate reiciendis amet aperiam ratione vero? A expedita, recusandae laboriosam deserunt aperiam nostrum, temporibus corrupti hic vero atque numquam nobis sed, magnam id ipsa quis animi quos pariatur alias quae! Accusamus labore non incidunt quasi debitis perspiciatis fugiat, excepturi exercitationem perferendis quas similique culpa impedit natus facilis hic dicta dolorum qui quis molestiae? Aperiam non delectus reprehenderit modi ipsa, necessitatibus fugit consequuntur maiores neque nihil velit provident, voluptates eius soluta accusamus ipsum harum id architecto sapiente, nemo ad rerum expedita? Quasi dolore dolores id omnis a.</p>
+          <Grid item xs={7}>
+            <Typography variant="h5">I'm a freelance developer located at Vancouver, BC. </Typography>
+            <Typography variant="h5">I'm passionate about creating <span className={styles.bold}>fast, responsive, creative and dynamic user experience.</span></Typography>
+            <Typography variant="h5" className={styles.skills}>Skills</Typography>
+            <Typography variant="body" className={styles.skillset}>HTML5, CSS3, SASS, JavaScript, Material-UI, BootStrap, React, Redux, Node, Express, WordPress, MySQL</Typography>
           </Grid>
         </Grid>
       </Box>
