e3a7188e9dc94bed04318baa2c4699e96079bf5b
Zahra <zbehfarshad@gmail.com>
2021-06-08 16:11:35 -0700

    Creating the contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout';
+import { Box, Grid, Typography } from '@material-ui/core';
+import styles from '../styles/contact.module.css';
+
+const Contact = () => {
+    return (
+        <Layout>
+            <Grid container>
+                <Grid item xs={4}>
+
+                </Grid>
+                <Grid item xs={8}></Grid>
+            </Grid>
+        </Layout>
+    )
+}
+
+export default Contact;
